c63914c1612e8636c6331a681060133e6701d6fe
st7105 <st7105@gmail.com>
2021-08-11 17:19:07 +0700

    Revert "Relogin"
    This reverts commit 77a0b3cc4a07c081b55f073cb4f5f4ed98fa6977.

--- torrt/rpc/utorrent.py
+++ torrt/rpc/utorrent.py
@@ -109,11 +109,6 @@ class UTorrentRPC(BaseRPC):
             response = self.client.request(
                 url=url, auth=(self.user, self.password), **request_kwargs)
 
-            if self.client.last_response.status_code == 400:
-                self.login()
-                response = self.client.request(
-                    url=url, auth=(self.user, self.password), **request_kwargs)
-
             if self.client.last_response.status_code != 200:
                 raise UTorrentRPCException(response.text.strip())
 
